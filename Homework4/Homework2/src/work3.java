cfbad56dd0e1871082eeb421eee17e6734f3bb04
roshkev <rosenpetov@abv.bg>
2015-11-04 18:51:42 +0200

    test

--- Homework4/Homework2/src/work3.java
+++ Homework4/Homework2/src/work3.java
@@ -10,25 +10,25 @@ public class work3 {
         int day = scanner.nextInt();
 
         switch (day) {
-            case 0:
+            case 1:
                 System.out.println("Monday");
                 break;
-            case 1:
+            case 2:
                 System.out.println("Tuesday");
                 break;
-            case 2:
+            case 3:
                 System.out.println("Wednesday");
                 break;
-            case 3:
+            case 4:
                 System.out.println("Thursday");
                 break;
-            case 4:
+            case 5:
                 System.out.println("Friday");
                 break;
-            case 5:
+            case 6:
                 System.out.println("Saturday");
                 break;
-            case 6:
+            case 7:
                 System.out.println("Sunday");
                 break;
             default:
